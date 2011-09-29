a156482ac98ce7d5c6647c05afb3ca0894507b51
Chad Pry <chad.pry@gmail.com>
2011-09-29 02:33:11 -0500

    + added initial gem files and setup

---
+++ lib/sinatra/initializers.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+require 'sinatra/base'
+
+module Sinatra
+  class Base
+    set(:config_directory, "config/initializers") unless respond_to?(:config_directory)
+  end
+
+  module Initializers
+    VERSION = '0.1.0' unless const_defined?(:VERSION)
+
+    def self.version
+      "Sinatra::Initializers v#{VERSION}"
+    end
+
+    def self.registered app
+      Dir["#{config_directory}/**/*.rb"].each do |file_path|
+        require File.join(Dir.pwd, file_path)
+      end
+    end
+  end
+end
+
