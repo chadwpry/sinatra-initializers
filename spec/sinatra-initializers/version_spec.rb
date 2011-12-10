6cf95a35f161cfa46f85247553367be8f1748334
Chad Pry <chad.pry@gmail.com>
2011-12-10 17:39:43 -0600

    + fix broken version test to 0.1.3

--- spec/sinatra-initializers/version_spec.rb
+++ spec/sinatra-initializers/version_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Sinatra::Initializers do
   it 'is the latest version' do
-    Sinatra::Initializers.version.should == '0.1.1'
+    Sinatra::Initializers.version.should == '0.1.3'
   end
 end
 
