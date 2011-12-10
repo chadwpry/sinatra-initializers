a5ca9ca591d87aa3b7149ce49019a63975daba13
Chad Pry <chad.pry@gmail.com>
2011-12-10 17:45:11 -0600

    + move to next version with correct specs

--- lib/sinatra-initializers/version.rb
+++ lib/sinatra-initializers/version.rb
@@ -1,6 +1,6 @@
 module Sinatra
   module Initializers
-    VERSION = '0.1.3' unless const_defined?(:VERSION)
+    VERSION = '0.1.4' unless const_defined?(:VERSION)
 
     def self.version
       VERSION
--- spec/sinatra-initializers/version_spec.rb
+++ spec/sinatra-initializers/version_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Sinatra::Initializers do
   it 'is the latest version' do
-    Sinatra::Initializers.version.should == '0.1.3'
+    Sinatra::Initializers.version.should == '0.1.4'
   end
 end
 
